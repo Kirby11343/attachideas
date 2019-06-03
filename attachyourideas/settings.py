dc5106e083281bded17bd3bf5559b8c2c2482e61
Bocharov Denis <kirby11343@gmail.com>
2019-06-03 19:37:45 +0300

    changes

--- attachyourideas/settings.py
+++ attachyourideas/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 import dj_database_url
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -173,5 +172,3 @@ EMAIL_USE_TLS = True
 # EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
-
-django_heroku.settings(locals())
