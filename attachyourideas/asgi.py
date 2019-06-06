2779882961bfcb286fc542ae0c7bb9f4cec29af4
Bocharov Denis <kirby11343@gmail.com>
2019-06-06 22:32:29 +0300

    123

--- attachyourideas/asgi.py
+++ attachyourideas/asgi.py
@@ -5,3 +5,4 @@ from channels.routing import get_default_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "attachyourideas.settings")
 django.setup()
 application = get_default_application()
+channel_layer = channels.asgi.get_channel_layer()
