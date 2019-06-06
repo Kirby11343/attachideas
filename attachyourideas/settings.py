9897477d9b0dd4847c046d33e8512b2fddf03d74
Bocharov Denis <kirby11343@gmail.com>
2019-06-06 23:28:21 +0300

    ch

--- attachyourideas/settings.py
+++ attachyourideas/settings.py
@@ -84,7 +84,7 @@ ASGI_APPLICATION = 'attachyourideas.routing.application'
 
 CHANNEL_LAYERS = {
     'default': {
-        'BACKEND': 'asgi_redis.RedisChannelLayer',
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
             "hosts": [os.environ.get('REDIS_URL', 'redis://localhost:6379')],
         },
